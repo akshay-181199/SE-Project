02ba55f00c78aabb27274547a30bef232f1fe770
SanjayKumarKKR <ramarthysanjay@gmail.com>
2020-01-22 21:48:08 +0530

    Added files

--- FacultyProfile/app/src/main/java/com/example/facultyprofile/Activity/ProfessorListActivity.java
+++ FacultyProfile/app/src/main/java/com/example/facultyprofile/Activity/ProfessorListActivity.java
@@ -1,18 +1,20 @@
 package com.example.facultyprofile.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.facultyprofile.Adapters.professorlistadapter;
+import com.example.facultyprofile.Listeners.OnObjectFetchListener;
 import com.example.facultyprofile.Listeners.OnObjectListFetchListener;
 import com.example.facultyprofile.Models.Professors;
 import com.example.facultyprofile.R;
 import com.example.facultyprofile.managers.databasehelper;
-import com.google.firebase.FirebaseApp;
 
 import java.util.ArrayList;
 
@@ -24,6 +26,8 @@ public class ProfessorListActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        String str = intent.getStringExtra("flag");
         setContentView(R.layout.professors_show_list);
         professorrecylerview = findViewById(R.id.professorlistRecyclerView);
         Professorlistadapter = new professorlistadapter(this);
@@ -38,20 +42,33 @@ public class ProfessorListActivity extends BaseActivity {
         professorrecylerview.setHasFixedSize(true);
         professorrecylerview.setLayoutManager(layoutManager);
         showProgress();
-        db.fetchallprofessors(new OnObjectListFetchListener() {
-            @Override
-            public void onListChanged(ArrayList list, boolean isEmpty) {
-                if (isEmpty){
-                    hideProgress();
-                    setContentView(R.layout.activity_main);
-                }
-                else {
+        if(str.equals("0")){
+            db.fetchoneprofessors(intent.getStringExtra("name"), new OnObjectFetchListener() {
+                @Override
+                public void onDataFetched(Object object) {
+                    ArrayList<Professors> list=new ArrayList<Professors>();
+                    list.add((Professors) object);
                     hideProgress();
                     Professorlistadapter.setProfessorsArrayList(list);
-
                 }
-            }
-        });
+            });
+        }
+        else{
+            db.fetchallprofessors(new OnObjectListFetchListener() {
+                @Override
+                public void onListChanged(ArrayList list, boolean isEmpty) {
+                    if (isEmpty){
+                        hideProgress();
+                        setContentView(R.layout.activity_main);
+                    }
+                    else {
+                        hideProgress();
+                        Professorlistadapter.setProfessorsArrayList(list);
+
+                    }
+                }
+            });
+        }
 
     }
 }
--- FacultyProfile/app/src/main/java/com/example/facultyprofile/Activity/SearchActivity.java
+++ FacultyProfile/app/src/main/java/com/example/facultyprofile/Activity/SearchActivity.java
@@ -2,20 +2,83 @@ package com.example.facultyprofile.Activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
 import com.example.facultyprofile.R;
 import com.google.firebase.FirebaseApp;
 
 
 
 public class SearchActivity extends BaseActivity {
-
+    EditText univ,stream,dep,name;
+    Spinner s1;
+    Button but;
+    String[] strings = {"FACULTY NAME","DEPARTMENT"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         FirebaseApp.initializeApp(getApplicationContext());
-        Intent intent = new Intent(this,ProfessorListActivity.class);
-        startActivity(intent);
+        univ= findViewById(R.id.UNIVERSITYNAME);
+        dep= findViewById(R.id.DEPARTMENT);
+        name= findViewById(R.id.FACULTYNAME);
+        stream=findViewById(R.id.STREAM);
+        but=findViewById(R.id.search);
+        dep.setVisibility(View.GONE);
+        univ.setEnabled(false);
+        stream.setEnabled(false);
+        s1=findViewById(R.id.course_name);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.course, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s1.setAdapter(adapter);
+        s1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position==0){
+                    name.setVisibility(View.VISIBLE);
+                    dep.setVisibility(View.GONE);
+                }
+                else{
+                    dep.setVisibility(View.VISIBLE);
+                    name.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(name.getText().toString().matches("")&&dep.getText().toString().matches("")){
+                    Toast.makeText(SearchActivity.this,"Please Enter Some Value",Toast.LENGTH_LONG).show();
+                }
+                else if(dep.getText().toString().matches("")){
+                     Intent intent = new Intent(SearchActivity.this,ProfessorListActivity.class);
+                     intent.putExtra("flag","0");
+                     intent.putExtra("name",name.getText().toString());
+                    startActivity(intent);
+                }
+                else{
+                    Intent intent = new Intent(SearchActivity.this,ProfessorListActivity.class);
+                    intent.putExtra("flag","1");
+                    intent.putExtra("dep",dep.getText());
+                    startActivity(intent);
+                }
+            }
+        });
+
+
     }
 
 }
