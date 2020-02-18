878fcbc1cf9b539ff76d02b19075fe6b6a745475
SanjayKumarKKR <ramarthysanjay@gmail.com>
2020-02-18 21:52:53 +0530

    testcase for negative too:)

--- FacultyProfile/app/src/androidTest/java/com/example/facultyprofile/ExpressoTesting.java
+++ FacultyProfile/app/src/androidTest/java/com/example/facultyprofile/ExpressoTesting.java
@@ -15,18 +15,22 @@ import com.example.facultyprofile.Activity.SearchActivity;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withSpinnerText;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.anything;
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 
 
 
@@ -147,5 +151,83 @@ public class ExpressoTesting {
         onView(withId(R.id.course_name)).perform(click());
         onData(anything()).atPosition(0).perform(click());
         onView(withId(R.id.course_name)).check(matches(withSpinnerText(containsString("FACULTY NAME"))));
+    }
+    @Test
+    public void Image_Analysis() {
+        // Type text and then press the button.
+        onView(withId(R.id.course_name)).perform(click());
+        onData(anything()).atPosition(2).perform(click());
+        onView(withId(R.id.course_name)).check(matches(withSpinnerText(containsString("INTERESTS"))));
+        onView(withId(R.id.INTEREST))
+                .perform(typeText("Image analysis"), closeSoftKeyboard());
+        onView(withId(R.id.search)).perform(click());
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(withId(R.id.professorlistRecyclerView))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        onView(withId(R.id.profilename)).check(matches(withText("Padmavathi S")));
+        onView(withId(R.id.selection)).perform(click());
+        onData(anything()).atPosition(0).perform(click());
+        onView(withId(R.id.selection)).check(matches(withSpinnerText(containsString("AREA OF INTEREST"))));
+        onData(anything()).inAdapterView(withId(R.id.areaofinterestlistview)).atPosition(0).perform(click());
+        onView(withId(R.id.areaofinterestlistview)).check(matches(isDisplayed()));
+        onView(withId(R.id.selection)).perform(click());
+        onData(anything()).atPosition(1).perform(click());
+        onView(withId(R.id.selection)).check(matches(withSpinnerText(containsString("PUBLICATIONS"))));
+        onView(withId(R.id.publicationslistview)).check(matches(isDisplayed()));
+    }
+    @Test
+    public void Hybrid_Test() {
+        // Type text and then press the button.
+
+       onView(withId(R.id.course_name)).perform(click());
+        onData(anything()).atPosition(2).perform(click());
+        onView(withId(R.id.course_name)).check(matches(withSpinnerText(containsString("INTERESTS"))));
+        onView(withId(R.id.INTEREST))
+                .perform(typeText("IoT"), closeSoftKeyboard());
+        onView(withId(R.id.search)).perform(click());
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(withId(R.id.professorlistRecyclerView))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(1, click()));
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        onView(withId(R.id.profilename)).check(matches(withText("Shriram K Vasudevan")));
+        onView(withId(R.id.selection)).perform(click());
+        onData(anything()).atPosition(0).perform(click());
+        onView(withId(R.id.selection)).check(matches(withSpinnerText(containsString("AREA OF INTEREST"))));
+        onData(anything()).inAdapterView(withId(R.id.areaofinterestlistview)).atPosition(0).perform(click());
+        onView(withId(R.id.areaofinterestlistview)).check(matches(isDisplayed()));
+        onView(withId(R.id.selection)).perform(click());
+        onData(anything()).atPosition(1).perform(click());
+        onView(withId(R.id.selection)).check(matches(withSpinnerText(containsString("PUBLICATIONS"))));
+        onView(withId(R.id.publicationslistview)).check(matches(isDisplayed()));
+    }
+    @Test
+    public void Negative() {
+        // Type text and then press the button.
+
+        onView(withId(R.id.course_name)).perform(click());
+        onData(anything()).atPosition(2).perform(click());
+        onView(withId(R.id.course_name)).check(matches(withSpinnerText(containsString("INTERESTS"))));
+        onView(withId(R.id.INTEREST))
+                .perform(typeText("Marana Mass"), closeSoftKeyboard());
+        onView(withId(R.id.search)).perform(click());
     }
 }
